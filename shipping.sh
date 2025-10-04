04a8cb74b38b4505231e03a9d71e2f1728fe4c32
bharath-gaveni <bharathgaveni@gmail.com>
2025-10-05 00:22:49 +0530

    script

---
+++ shipping.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+N="\e[0m"
+R="\e[0;31m"
+G="\e[0;32m"
+Y="\e[0;33m"
+Dir_name=$PWD
+Host_name=mysql.bharathgaveni.fun
+
+id=$(id -u)
+
+if [ $id -ne 0 ]; then
+    echo -e "$R please execute the script $0 with root user privilage access $N"
+    exit 1
+fi
+
+log_folder=/var/log/shell-roboshop-PT
+script_name=$(echo $0 | cut -d "." -f1)
+log_file=$log_folder/$script_name.log
+start_time=$(date +%s)
+mkdir -p $log_folder
+echo "Script $0 execution started at time:$(date)" | tee -a $log_file
+
+validate() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is $R Failed $N" | tee -a $log_file
+        exit 1
+    else
+        echo -e "$2 is $G success $N" | tee -a $log_file
+    fi
+}
+
+dnf install maven -y &>>$log_file
+validate $? "installing maven"
+
+mkdir -p /app &>>$log_file
+validate $? "creating app directory to place our application"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$log_file
+validate $? "dowloading the zipped code to temp location"
+
+cd /app &>>$log_file
+validate $? "changing to app directory"
+
+rm -rf /app/* &>>$log_file
+validate $? "Removing the existing shipping code in app directory"
+
+unzip /tmp/shipping.zip &>>$log_file
+validate $? "unzipping the shipping code in app directory location"
+
+cd /app &>>$log_file
+validate $? "changing to app directory"
+
+mvn clean package &>>$log_file
+validate $? "downloading the dependencies"
+
+mv target/shipping-1.0.jar shipping.jar
+validate $? "moving shipping.jar "
+
+id roboshop &>>$log_file
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    echo -e "roboshop user created $G Successfully $N" | tee -a $log_file
+else
+    echo -e "user already exists $Y SKIPPING $N" | tee -a $log_file
+fi
+
+cp $Dir_name/shipping.service /etc/systemd/system/shipping.service &>>$log_file
+validate $? "copying shipping.service file and setting up systemd service"
+
+systemctl daemon-reload &>>$log_file
+validate $? "reloading the daemon to recongnize the new service created"
+
+systemctl enable shipping &>>$log_file
+validate $? "enabling shipping"
+
+systemctl start shipping &>>$log_file
+validate $? "started the shipping"
+
+dnf install mysql -y &>>$log_file
+validate $? "installing the mysql client to connect with mysql db"
+
+mysql -h $Host_name -uroot -pRoboShop@1 -e 'use cities' &>>$log_file
+if [ $? -ne 0 ]; then
+    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/schema.sql
+    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/app-user.sql
+    mysql -h $Host_name -uroot -pRoboShop@1 < /app/db/master-data.sql
+else
+    echo -e "Shipping data is already loaded $Y SKIPPING $N"
+fi
+
+systemctl restart shipping
+validate $? "Resart shipping"
+
+end_time=$(date +%s)
+total_time=$(($end_time-$start_time))
+echo "Total time taken to execute the script $0 is:$total_time seconds" | tee -a $log_file
